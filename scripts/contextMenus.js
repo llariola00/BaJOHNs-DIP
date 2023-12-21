ff1768d3a6665ce4c86b67acfa4bb813beb0a490
llariola00 <llcariola@usep.edu.ph>
2023-12-22 04:24:13 +0800

    modal test

--- scripts/contextMenus.js
+++ scripts/contextMenus.js
@@ -10,19 +10,18 @@ chrome.runtime.onInstalled.addListener(function () {
 chrome.contextMenus.onClicked.addListener(genericOnClick);
 
 // A generic onclick callback function.
+// A generic onclick callback function.
 function genericOnClick(info) {
     console.log("info: ", info);
-    switch (info.menuItemId) {
-        case "image":
-            // image item function
-            console.log("Image item clicked. Media Type:", info.mediaType);
-            break;
-        case "video":
-            // video item function
-            console.log("Video item clicked. Media Type:", info.mediaType);
-            break;
-        default:
-            // Standard context menu item function
-            console.log("Standard context menu item clicked.");
+    if (info.menuItemId === "image") {
+        // image item function
+        console.log("Image item clicked. Media Type:", info.mediaType);
+        // Send a message to the content script to show the modal
+        chrome.tabs.query(
+            { active: true, currentWindow: true },
+            function (tabs) {
+                chrome.tabs.sendMessage(tabs[0].id, { command: "showModal" });
+            }
+        );
     }
 }
