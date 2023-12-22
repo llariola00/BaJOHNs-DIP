cd0d9c4e3a924005c6afd535aa76609b52b805ca
llariola00 <llcariola@usep.edu.ph>
2023-12-23 04:38:26 +0800

    Initial buttons for image processes. Initial buttons for Apply, Download, Clipboard. Partial functionality for buttons.

--- scripts/imageProcess.js
+++ scripts/imageProcess.js
@@ -1,14 +1,94 @@
 let imgSource = document.getElementById("imageSrc");
 let canvasOutput = document.getElementById("canvasOutput");
+let btnApply = document.getElementById("btn-apply"); // The button for applying the grayscale filter
+let btnDownload = document.getElementById("btn-download");
+let btnClipboard = document.getElementById("btn-clipboard");
+
+let mat;
+let btnGroup = document.querySelector(".btn-group"); // Select the btn-group div
+let imgProcess;
 
 imgSource.onload = function () {
-    let mat = cv.imread(imgSource);
+    mat = cv.imread(imgSource);
+};
 
-    // Convert the image to grayscale
-    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY);
+btnApply.onclick = function () {
+    if (mat) {
+        switch (imgProcess) {
+            case "Grayscale":
+                try {
+                    cv.cvtColor(mat, mat, cv.COLOR_RGBA2GRAY);
+                    cv.imshow(canvasOutput, mat);
+                    //mat.delete(); // Clean up the memory
+                    console.log("Grayscale filter is applied.");
+                } catch (err) {
+                    console.log(err);
+                }
+                break;
 
-    cv.imshow(canvasOutput, mat);
-    mat.delete();
+            case "Blur":
+                let src = mat;
+                let dst = new cv.Mat();
+                let ksize = new cv.Size(9, 9);
+                let anchor = new cv.Point(-1, -1);
+                cv.blur(src, dst, ksize, anchor, cv.BORDER_DEFAULT);
+                cv.imshow(canvasOutput, dst);
+                // src.delete();
+                // dst.delete();
+                console.log("Blur is applied.");
+                break;
+
+            case "2D Convolution":
+                console.log("2D Convolution is applied.");
+                break;
+
+            case "Resize":
+                console.log("Resize is applied.");
+                break;
+
+            case "Rotate":
+                console.log("Rotate is applied.");
+                break;
+            default:
+                console.log("No valid image processing operation selected.");
+        }
+    }
+};
+
+// Add an event listener to the btn-group div
+btnGroup.addEventListener("change", function (event) {
+    if (event.target.type === "radio") {
+        console.log("Selected button:", event.target.value);
+        imgProcess = event.target.value;
+    }
+});
+
+// Function to download the processed image
+btnDownload.onclick = function () {
+    let link = document.createElement("a");
+    let dataUrl = canvasOutput.toDataURL();
+    link.href = dataUrl;
+    link.download = "processed_image.png";
+    // Trigger a click event on the link to start the download
+    link.click();
+};
+
+// Function to copy the processed image to the clipboard
+btnClipboard.onclick = function () {
+    let dataUrl = canvasOutput.toDataURL();
+    fetch(dataUrl)
+        .then((res) => res.blob())
+        .then((blob) => {
+            let clipboardItem = new ClipboardItem({ "image/png": blob });
+            navigator.clipboard
+                .write([clipboardItem])
+                .then(() => {
+                    console.log("Image copied to clipboard");
+                    alert("Image copied to clipboard"); // Add this line
+                })
+                .catch((err) => console.error(err));
+        })
+        .catch((err) => console.error(err));
 };
 
 var Module = {
